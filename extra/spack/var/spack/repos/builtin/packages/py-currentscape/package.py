db0e65c4ff506c6da0a0696770e2fc872bed61ef
Jaquier Aurélien Tristan <aurelien.jaquier@epfl.ch>
2020-10-16 15:38:31 +0200

    Updated version
    Change-Id: I69da7e7b6a58f8d0df3722c13ea4b89686b56548

--- extra/spack/var/spack/repos/builtin/packages/py-currentscape/package.py
+++ extra/spack/var/spack/repos/builtin/packages/py-currentscape/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -6,23 +6,18 @@
 from spack import *
 
 
-# replace all 'x-y' with 'xY' (e.g. 'Py-morph-tool' -> 'PyMorphTool')
 class PyCurrentscape(PythonPackage):
     """Module to easily plot currentscape."""
 
-    homepage = "https://bbpteam.epfl.ch/documentation/projects/currentscape"
+    homepage = "https://bbpcode.epfl.ch/code/#/admin/projects/cells/currentscape"
+    url = "ssh://bbpcode.epfl.ch/cells/currentscape"
     git = "ssh://bbpcode.epfl.ch/cells/currentscape"
 
     version("develop", branch="master")
-    version("0.0.1.dev0", tag="currentscape-v0.0.1.dev0", preferred=True)
+    version("0.0.1", tag="currentscape-v0.0.1")
 
-    depends_on(
-        "py-setuptools", type="build"
-    )  # type=('build', 'run') if specifying entry points in 'setup.py'
-
+    depends_on("py-setuptools", type="build")
     depends_on("py-numpy", type="run")
     depends_on("py-matplotlib", type="run")
     depends_on("py-scipy", type="run")
 
-    # for all 'foo>=X' in 'install_requires' and 'extra_requires':
-    # depends_on('py-foo@<min>:')

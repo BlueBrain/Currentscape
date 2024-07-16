c3dcade9b9a5efb91533d0b5ebc16f6467c33d21
Jaquier Aurélien Tristan <aurelien.jaquier@epfl.ch>
2024-07-16 14:53:34 +0200

    black fix
    Change-Id: I44f2477f5a08378c54e5202c49bf7fec1df9afb5

--- currentscape/currentscape.py
+++ currentscape/currentscape.py
@@ -66,7 +66,11 @@ def create_figure(voltage, currs, c, ions):
     # put into classes
     if c["current"]["autoscale_ticks_and_ylim"]:
         autoscale_ticks_and_ylim(c, currs.pos_sum, currs.neg_sum)
-    if ions.data is not None and ions.data.size and c["ions"]["autoscale_ticks_and_ylim"]:
+    if (
+        ions.data is not None
+        and ions.data.size
+        and c["ions"]["autoscale_ticks_and_ylim"]
+    ):
         autoscale_ticks_and_ylim(c, np.max(ions.data), abs(np.min(ions.data)), "ions")
 
     rows_tot = get_rows_tot(c, ions)
